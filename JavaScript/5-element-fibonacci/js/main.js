f59395f58d8469af140b69a1b9c8505d4f7937d4
Matylda Czuba <matylda.czuba@pm.me>
2018-10-09 19:55:35 +0200

    1

---
+++ JavaScript/5-element-fibonacci/js/main.js
@@ -0,0 +1,13 @@
+function fib(n) {
+    
+    let a = 0;
+    let suma = 0;
+//    let suma2 = 0;
+    
+    for (let i = 0; i <= n; i++) {
+        suma = suma + i;
+        console.log(suma);
+    }
+}
+
+fib(45);
