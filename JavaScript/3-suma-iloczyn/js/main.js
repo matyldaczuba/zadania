1e4edb3d7a37d2d843a5e164ea3ecd86dffe4361
Matylda Czuba <matylda.czuba@pm.me>
2018-10-12 23:10:52 +0200

    done

--- JavaScript/3-suma-iloczyn/js/main.js
+++ JavaScript/3-suma-iloczyn/js/main.js
@@ -1,28 +1,43 @@
-//function sumaIloczyn(numArray) {
-//
-//    console.log(numArray);
-//    numArray = [1, 2, 3, 4, 5, 6];
-//    let wynik = numArray.forEach(function (element, index) {
-//        let ilosc = numArray.length;
-//        
-//        //        let suma = numArray[index] + numArray[];
-//        //        console.log(suma);
-//
-//    });
-//
-//    //}
-//
-//    //    let liczby = [1, 2, 3, 4, 5, 6];
-//    //    let ilosc = liczby.length;
-//    //    for (let i = 0; i >= ilosc, i++) {
-//    //        let suma = liczby[i] + liczby[i++];
-    //    }
+function sumaIloczyn1(numArray) {
 
-    //sumaIloczyn(liczby);
-function sumaIloczyn(tablica){
-    console.log(tablica);
-    tablica.forEach()
+    let suma = 0;
+    let iloczyn = 1;
+    let wynik = numArray.forEach(function (element, index) {
+
+        suma = suma + numArray[index];
+        iloczyn = iloczyn * numArray[index];
+
+
+    });
+    console.log("Suma z forEach = " + suma);
+    console.log("Iloczyn z forEach = " + iloczyn);
+
+
 }
 
 let liczby = [1, 2, 3, 4, 5, 6];
-sumaIloczyn(liczby);
+
+sumaIloczyn1(liczby);
+
+
+
+
+function sumaIloczyn2(tablica) {
+
+    let length = tablica.length;
+    let suma = 0;
+    let iloczyn = 1;
+
+    for (let i = 0; i < length; i++) {
+
+        suma = suma + tablica[i];
+        iloczyn = iloczyn * tablica[i];
+
+    }
+    
+    console.log("Suma z for = " + suma);
+    console.log("Iloczyn z for = " + iloczyn);
+
+}
+
+sumaIloczyn2(liczby);
