c0626ab42a836803f557dae2a0232e10127e6b54
Matylda Czuba <matylda.czuba@pm.me>
2018-10-12 23:10:39 +0200

    poprawki i skonczone

--- JavaScript/2-sortuj-litery-stringa/js/main.js
+++ JavaScript/2-sortuj-litery-stringa/js/main.js
@@ -1,11 +1,10 @@
-let string = "Akademia108";
-
-console.log("Przed sortowaniem: " + string);
 function sort(text){
     let array = text.split("");
     let sortedArray = array.sort();
-    
-    console.log("Po sortowaniu: " + array.join(""));
+    return(array.join(""));
 }
 
-sort(string);
+let string = "Akademia108";
+console.log("Przed sortowaniem: " + string);
+console.log("Po sortowaniu: " + sort(string));
+
