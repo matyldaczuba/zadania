4c6da1050ad6be088f872f2be97d803283669b00
Matylda Czuba <matylda.czuba@pm.me>
2018-10-19 00:57:51 +0200

    done

--- JavaScript/5-element-fibonacci/js/main.js
+++ JavaScript/5-element-fibonacci/js/main.js
@@ -1,14 +1,28 @@
+//var i;
+//var fib = []; // Initialize array!
+//
+//fib[0] = 0;
+//fib[1] = 1;
+//for (i = 2; i <= 10; i++) {
+//  // Next fibonacci number = previous + one before previous
+//  // Translated to JavaScript:
+//  fib[i] = fib[i - 2] + fib[i - 1];
+//  console.log(fib[i]);
+//}
+
+
 function fib(n) {
     
-    let a = 0;
-    let suma = 0;
-//    let suma2 = 0;
+    let fibArray = [];
+    fibArray[0] = 0;
+    fibArray[1] = 1;
     
-    for (let i = 0; i <= n; i++) {
-        suma = suma + i;
-        
-        console.log(suma);
+    for (let i = 2; i <= n; i++) {
+        fibArray[i] = fibArray[i - 2] + fibArray[i - 1];
+//        console.log(fibArray[i]);
     }
+    console.log(fibArray);
+    return fibArray[n];
 }
 
-fib(45);
+console.log(fib(45));
