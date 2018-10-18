1fb53725b8254d12243cac757dfc2eb4f875784b
Matylda Czuba <matylda.czuba@pm.me>
2018-10-19 00:34:56 +0200

    done

--- JavaScript/8-sortowanie-babelkowanie/js/main.js
+++ JavaScript/8-sortowanie-babelkowanie/js/main.js
@@ -3,37 +3,28 @@ let tablica = [12, 67, 34, 23.01, 24, 2, 56, 8, 0x10, 23.02, 78, 34, 1e2, 45, 67
 console.log(`Przed sortowaniem: ${tablica}`);
 
 function sort(array){
-    let n = array.length;
-    do {
-        for(let i = 0; i < n-1; i++){
-            if(array[i] > array [i+1]){
-                let tmp = array[i+1];
-                array[i+1] = array[i];
-                array[i] = tmp;
+    
+    let length = array.length;
+    let tmp;
+    for(let i = 0; i < length-1; i++) {
+        
+        for(let j = 0; j < length-1-i; j++){
+            
+            if(array[j] > array[j+1]){
                 
-                console.log("dzzialaa");
-
+                tmp = array[j+1];
+                array[j+1] = array[j];
+                array[j] = tmp;
+                
             }
         }
-        console.log(array);
+        
     }
-    while(n>1);
-
+   
+    console.log(`Po sortowaniu: ${array}`);
 }
 
-let posortowanaTablica = sort(tablica);
+sort(tablica);
 
-console.log(`Po sortowaniu: ${posortowanaTablica}`);
-//
-//procedure bubbleSort( A : lista elementów do posortowania )
-//  n = liczba_elementów(A)
-//   do
-//    for (i = 0; i < n-1; i++) do:
-//      if A[i] > A[i+1] then
-//        swap(A[i], A[i+1])
-//      end if
-//    end for
-//    n = n-1
-//  while n > 1
-//end procedure
+
 
