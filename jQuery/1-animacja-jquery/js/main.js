452d3cc4e87927d6cf46eba6d140dce5afa82a8c
Matylda Czuba <matylda.czuba@pm.me>
2018-10-23 20:23:09 +0200

    done

--- jQuery/1-animacja-jquery/js/main.js
+++ jQuery/1-animacja-jquery/js/main.js
@@ -1,4 +1,33 @@
-// spraawdz cy link dobry
+let btn = $('button');
+let square = $('.square');
 
 
+square.css({
+    'background': 'red',
+    'width': '200px',
+    'height': '200px',
+    'margin-top': '20px',
+    'position': 'relative'
+});
 
+btn.click(function () {
+    console.log('Guzik został naciśnięty.');
+    square.animate({
+        'right': '-100px',
+        'width': '100px',
+        'height': '100px',
+
+    }, 3000, function () {
+
+        square.css({
+            'background': 'blue',
+            'transition': '5s'
+        });
+
+        window.setTimeout(function () {
+            square.append("<h2>Animacja zakończona</h2>");
+
+        }, 5000);
+
+    })
+});
